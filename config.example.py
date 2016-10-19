b1c5b4beb605f604e7550fb9a9a5554439187681
cassac <hicassac@gmail.com>
2016-10-18 22:55:16 -0700

    updates

--- config.example.py
+++ config.example.py
@@ -1,3 +1,10 @@
-username='MyGithubUsername'
-password='MyGithubPassword'
+# .csv filename
 filename='MyFileName.csv'
+
+# Github credentials
+gh_username='MyGithubUsername'
+gh_password='MyGithubPassword'
+
+# LinkedIn credentials
+li_username='MyLinkedInUsername'
+li_password='MyLinkedInPassword'
