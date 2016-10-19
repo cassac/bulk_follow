0282238db4af67828ea9eea16f469f45f30f7744
cassac <hicassac@gmail.com>
2016-10-18 23:54:26 -0700

    implements helper function

--- links.py
+++ links.py
@@ -1,5 +1,6 @@
 import csv
 from config import filename
+from url_helper import url_checker
 
 github_urls = []
 linkedin_urls = []
@@ -16,6 +17,7 @@ with open(filename, 'r') as f:
         # .csv file may be be different. 
         github_url = row[2] # Adjust column if needed 
         if (len(github_url)):
+          github_url = url_checker(github_url)
           github_urls.append(github_url)
         else:
           print('# ' + row[0] + ' ' + row[1] + "'s github is blank")
@@ -23,6 +25,7 @@ with open(filename, 'r') as f:
         # .csv file may be be different. 
         linkedin_url = row[3] # Adjust column if needed 
         if (len(linkedin_url)):
+            linkedin_url = url_checker(linkedin_url)
             linkedin_urls.append(linkedin_url)
         else:
             print('# ' + row[0] + ' ' + row[1] + "'s linkedin is blank")
