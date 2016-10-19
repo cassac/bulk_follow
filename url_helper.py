8e83a9e29c18515feb7042266cb694fb2f3a4f90
cassac <hicassac@gmail.com>
2016-10-18 23:54:06 -0700

    use fully qualified urls

---
+++ url_helper.py
@@ -0,0 +1,15 @@
+urls = [
+'http://shouldhavehttps.com/in/dude',
+'https://shouldbeok.com/in/girl',
+'thisneedsstuff.com/in/some/thing',
+'www.nogo.com/something',
+'www.nope.com/in/connect',
+'https://www.thisisok.com/myname']
+
+def url_checker(url):
+  first_five = url[:5]
+  if first_five != 'https://www':
+    part1 = url.split('//')[-1]
+    part2 = part1.split('www.')[-1]
+    url = 'https://www.' + part2
+  return url
