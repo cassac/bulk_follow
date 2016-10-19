8e724b69924daeb697637663b764f412e53f5ab4
cassac <hicassac@gmail.com>
2016-10-18 22:55:40 -0700

    updates syntax

--- follow.py
+++ follow.py
@@ -3,15 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from links import github_urls
-from config import username, password
+from config import gh_username, gh_password
 
 # Initiate webdriver
 driver = webdriver.Chrome()
 # Navigate to userpage and login
-driver.get("https://github.com/" + username)
+driver.get("https://github.com/" + gh_username)
 driver.find_element_by_link_text('Sign in').click()
-driver.find_element_by_name('login').send_keys(username)
-driver.find_element_by_name('password').send_keys(password)
+driver.find_element_by_name('login').send_keys(gh_username)
+driver.find_element_by_name('password').send_keys(gh_password)
 driver.find_element_by_name('commit').click()
 
 # Iterate through friends
